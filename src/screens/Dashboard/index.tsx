6016037a278829969052a11b0e41c6b7ca04d5d4
gabwestside <gabrielnfl13@gmail.com>
2021-10-20 12:47:41 -0300

    Introduzindo o Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 
-import { Container, Header } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/60760967?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Gabriel</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,51 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
     flex: 1;
 
-    background-color: ${({ theme }) => theme.colors.background };
+    background: ${({ theme }) => theme.colors.background};
 `;
 
 export const Header = styled.View`
     width: 100%;
-    height: 278px;
+    height: ${RFPercentage(42)}px;
 
-    background-color: ${({ theme }) => theme.colors.primary }
+    background-color: ${({ theme }) => theme.colors.primary};
+
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const UserWrapper = styled.View`
+    width: 100%;
+    padding: 0 24px;
+`;
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Photo = styled.Image`
+    width: ${RFValue(55)}px;
+    height: ${RFValue(55)}px;
+
+    border-radius: 50%;
+`;
+
+export const User = styled.View`
+    margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+
 `;
